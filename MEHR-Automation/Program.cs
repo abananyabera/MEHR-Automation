2a56f2c8f648956ff504a5cf946e9d4b979cd26e
Tharun Sai <tharun.sai@corteva.com>
2024-02-02 10:56:38 +0530

    changes made on 2/2/2024

--- MEHR-Automation/Program.cs
+++ MEHR-Automation/Program.cs
@@ -142,46 +142,277 @@ namespace MEHR_Automation
 
             #endregion
 
+            //Read csv file Dataload_77 csv file
+            int lineCount = CountLinesInCsvFile(@"C:\Users\kwr579\Desktop\AUTOMATION\DataLoad_77_AfterTriage.csv");
+            Console.WriteLine("Number of lines in the file: " + lineCount);
+
             #region MyRegion
+            int comparisioncount = 0;
             string Query3 = "Select count (*), datasource, datasourceid from tbl_Employees_Import group by datasource,datasourceid order by datasourceid";
             SqlDataReader datareader = ExecuteQuery(Query3, sqlconnection);
             while (datareader.Read())
             {
+                
+                comparisioncount = comparisioncount +(int)datareader[0];
                 Console.WriteLine(datareader[0] + "|" + datareader[1] + "|" + datareader[2]);
+                
+
+            }
+            Console.WriteLine(comparisioncount);
+            if (lineCount == comparisioncount)
+            {
+                Console.WriteLine("Tharun Sai");
+            }
+            else
+            {
+                Console.WriteLine("Kanna");
             }
             Console.WriteLine("------------------Query3 is executed----------------");
             #endregion
 
             Console.WriteLine("Please Disable the jobs: \n 1. UserID Synch Job \n 2. Modification Synch Job \n Please press Y to continue");
             char chkJobs = Console.ReadLine()[0];
-            Console.WriteLine(chkJobs);
+            
 
-            if (chkJobs == 'Y')
+            if (chkJobs == 'Y' || chkJobs == 'y')
             {
                 Console.WriteLine("\n\n\nJob disabled successfully\n\n\n");
+
                 #region MyRegion
+                Console.WriteLine("\nQuery4 is started\n");
                 string Query4 = "select uniqueid,count(uniqueid),datasourceid from tbl_employees_import group by uniqueid,datasourceid having count(uniqueid)>1";
                 SqlDataReader datareader4 = ExecuteQuery(Query4, sqlconnection);
-                while (datareader4.Read())
+                if (!datareader4.HasRows)
                 {
-                    Console.WriteLine(datareader[0] + "|" + datareader[1] + "|" + datareader[2]);
+                    while (datareader4.Read())
+                    {
+                        Console.WriteLine(datareader4[0] + "|" + datareader4[1] + "|" + datareader4[2]);
+                    }
+
+                    Console.WriteLine("\n\nQuery4 is executed\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadLine();
+                }
+                #endregion
+
+
+                #region MyRegion
+                Console.WriteLine("\nQuery5 is started\n");
+                string Query5 = "IF OBJECT_ID('tbl_Employees_Import_Excluded') IS NOT NULL DROP TABLE tbl_Employees_Import_Excluded";
+                SqlDataReader datareader5= ExecuteQuery(Query5, sqlconnection);
+                if (!datareader5.HasRows)
+                {
+                    while (datareader5.Read())
+                    {
+                        Console.WriteLine(datareader5[0]);
+                    }
+                    Console.WriteLine("\n\nQuery5 is Executed\n\n");
+                }
+                else 
+                { 
+
+                    Console.WriteLine("Error");
+                    Console.ReadLine();
+                }
+                #endregion
+
+
+                #region MyRegion
+                Console.WriteLine("\nQuery6 is started\n");
+                string Query6 = "select b.* into tbl_Employees_Import_Excluded from WorkdayIntegratedEmployees a, tbl_employees_import b where me_uniqueID=b.uniqueid and a.datasourceid=b.datasourceid";
+                SqlDataReader datareader6 = ExecuteQuery(Query6, sqlconnection);
+                if (!datareader6.HasRows)
+                {
+                    while (datareader6.Read())
+                    {
+                        Console.WriteLine(datareader6[0] + "|" + datareader6[1]);
+                    }
+                    Console.WriteLine("\n\nQuery6 is Executed\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadLine();
+                }
+                #endregion
+
+
+                #region MyRegion
+                Console.WriteLine("\nQuery7 is started\n");
+                string Query7 = "DELETE b from WorkdayIntegratedEmployees a, tbl_employees_import b where me_uniqueID=b.uniqueid and a.datasourceid=b.datasourceid";
+                SqlDataReader datareader7 = ExecuteQuery(Query7, sqlconnection);
+                if (!datareader7.HasRows)
+                {
+
+                    while (datareader7.Read())
+                    {
+                        Console.WriteLine(datareader7[0] + "|" + datareader7[1]);
+                    }
+                    Console.WriteLine("\nQuery7 is Executed\n");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadLine();
+                }
+                #endregion
+
+                //#region MyRegion
+                //Console.WriteLine("\n stored procedure proc_Pre_Update_Processing started\n");
+                //string Query8 = "exec proc_Pre_Update_Processing";
+                //SqlDataReader datareader8 = ExecuteQuery(Query8, sqlconnection);
+                //while (datareader8.Read())
+                //{
+                //    Console.WriteLine(datareader8[0] + "|" + datareader8[1]);
+                //}
+                //Console.WriteLine("\nstored procedure proc_Pre_Update_Processing is Executed\n");
+                //#endregion
+
+                //#region MyRegion
+                //Console.WriteLine("\n stored procedure procProcessEmployeeUpdates started\n");
+                //string Query9 = "exec procProcessEmployeeUpdates";
+                //SqlDataReader datareader9 = ExecuteQuery(Query9, sqlconnection);
+                //while (datareader9.Read())
+                //{
+                //    Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                //}
+                //Console.WriteLine("\nstored procedure procProcessEmployeeUpdates is Executed\n");
+                //#endregion
+
+                #region MyRegion
+                Console.WriteLine("\n count of tbl_Employees_Import_Add is started \n");
+                string Query10 = "select count (*) from tbl_Employees_Import_Add";
+                SqlDataReader datareader10 = ExecuteQuery(Query10, sqlconnection);
+                while (datareader10.Read())
+                {   
+                    Console.WriteLine(datareader10[0]);
+                }
+                Console.WriteLine("\n count of tbl_Employees_Import_Add is completed \n");
+               
+                #endregion
+
+
+                #region MyRegion
+                Console.WriteLine("\n count of tbl_Employees_Import_Add_Deleted is started \n");
+                string Query11 = "select count (*) from tbl_Employees_Import_Add_Deleted";
+                SqlDataReader datareader11 = ExecuteQuery(Query11, sqlconnection);
+                while (datareader11.Read())
+                {
+                    Console.WriteLine(datareader11[0]);
+                }
+                Console.WriteLine("\n count of tbl_Employees_Import_Add_Deleted is completed \n");
+                #endregion
+
+                #region MyRegion
+                Console.WriteLine("\n count of tbl_Employees_Import_Remove is started \n");
+                string Query12 = "select count (*) from tbl_Employees_Import_Remove";
+                SqlDataReader datareader12 = ExecuteQuery(Query12, sqlconnection);
+                while (datareader12.Read())
+                {
+                    Console.WriteLine(datareader12[0]);
+                }
+                Console.WriteLine("\n count of tbl_Employees_Import_Remove is completed \n");
+                #endregion
+
+                #region MyRegion
+                Console.WriteLine("\n distinct(fieldname) of tbl_Employees_Import_Changed is started \n");
+                string Query13 = "Select distinct(fieldname) from tbl_Employees_Import_Changed";
+                SqlDataReader datareader13 = ExecuteQuery(Query13, sqlconnection);
+                while (datareader13.Read())
+                {
+                    string fieldValue = datareader13["fieldname"].ToString();
+                    Console.WriteLine(fieldValue);
+                    
+                }
+                Console.WriteLine("\n distinct(fieldname) of tbl_Employees_Import_Changed is completed\n");
+                #endregion
+
+
+                
+                int count = 0;
+                while (count < 4)
+                {
+                    Console.WriteLine("select the fieldname that need to be executed \n 1.orig_epassid \n 2.orig_internet_email \n 3.orig_first \n 4.orig_last \n");
+                    int admin_student = int.Parse(Console.ReadLine());
+                    switch (admin_student)
+                    {
+                        case 1:
+                            Console.WriteLine("\n orig_epassid is started \n");
+                            string Query14 = "Select a.orig_epassid,b.epassid,a.orig_first,b.first,a.orig_last,b.last,a.orig_middle,b.middle,a.orig_internet_email,b.internet_email,a.orig_site,b.site from dbo.tbl_Employees_Import_Changed A inner join dbo.tbl_employees_stage1_Hold B on A.masterid = b.masterid where a.fieldname = 'orig_epassid'";
+                            SqlDataReader datareader14 = ExecuteQuery(Query14, sqlconnection);
+                            while (datareader14.Read())
+                            {
+                                Console.WriteLine("Tharun Sai");
+                            }
+                            Console.WriteLine("\n orig_epassid is completed \n");
+                            break;
+                        case 2:
+                            Console.WriteLine("\n orig_internet_email is started \n");
+                            string Query15 = "Select a.orig_epassid,b.epassid,a.orig_first,b.first,a.orig_last,b.last,a.orig_middle,b.middle,a.orig_internet_email,b.internet_email,a.orig_site,b.site from \r\ndbo.tbl_Employees_Import_Changed A inner join dbo.tbl_employees_stage1_Hold B on A.masterid = b.masterid\r\nwhere a.fieldname = 'orig_internet_email'";
+                            SqlDataReader datareader15 = ExecuteQuery(Query15, sqlconnection);
+                            while (datareader15.Read())
+                            {
+                                Console.WriteLine("Kanna");
+                            }
+                            Console.WriteLine("\n orig_internet_email is completed \n");
+                            break;
+                        case 3:
+                            Console.WriteLine("\n orig_first is started \n");
+                            string Query16 = "Select a.orig_epassid,b.epassid,a.orig_first,b.first,a.orig_last,b.last,a.orig_middle,b.middle,a.orig_internet_email,b.internet_email,a.orig_site,b.site from \r\ndbo.tbl_Employees_Import_Changed A inner join dbo.tbl_Employees_Stage1_Hold B on A.masterid = b.masterid\r\nwhere a.fieldname = 'orig_first'";
+                            SqlDataReader datareader16 = ExecuteQuery(Query16, sqlconnection);
+                            while (datareader16.Read())
+                            {
+                                Console.WriteLine("Tharun");
+                            }
+                            Console.WriteLine("\n orig_first is completed \n");
+                            break;
+                        case 4:
+                            Console.WriteLine("\n orig_last is started \n");
+                            string Query17 = "Select a.orig_epassid,b.epassid,a.orig_first,b.first,a.orig_last,b.last,a.orig_middle,b.middle,a.orig_internet_email,b.internet_email,a.orig_site,b.site from \r\ndbo.tbl_Employees_Import_Changed A inner join dbo.tbl_Employees_Stage1_Hold B on A.masterid = b.masterid\r\nwhere a.fieldname = 'orig_last'";
+                            SqlDataReader datareader17 = ExecuteQuery(Query17, sqlconnection);
+                            while (datareader17.Read())
+                            {
+                                Console.WriteLine("Kanna Sai");
+                            }
+                            Console.WriteLine("\n orig_last is completed \n");
+                            break;
+                        default:
+                            Environment.Exit(0);
+                            break;
+
+                    }
+                    count++;
                 }
 
-                Console.WriteLine("------------------Query4 is executed----------------");
+                
 
-                #endregion
+
+
+
+
+
 
                 sqlconnection.Close();
 
 
                 //Read Excel  people Report File
-                 ReadExcelFile();
+                //ReadExcelFile();
+
+                Console.ReadLine();
+                
+                
+                
 
             }
             else
             {
                 Console.WriteLine("You have not disabled the job.");
             }
+
+
 
         }
 
@@ -200,31 +431,52 @@ namespace MEHR_Automation
 
         }
 
-        public static void ReadExcelFile()
-        {
-            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
-            app.Visible = true;
+        //public static void ReadExcelFile()
+        //{
+        //    Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+        //    app.Visible = true;
 
-            string path = "C:\\Users\\kwr579\\Desktop\\AUTOMATION\\People Report_1215.xlsx";
-            Workbook wb;
-            Worksheet ws;
+        //    string path = "C:\\Users\\kwr579\\Desktop\\AUTOMATION\\People Report_1215.xlsx";
+        //    Workbook wb;
+        //    Worksheet ws;
+
+        //    try
+        //    {
+        //        wb = app.Workbooks.Open(path);
+        //        ws = wb.Worksheets["sheet1"];
+
+        //        string cellData = " " + ws.Range["A1"].Value;
+        //        Console.WriteLine(cellData);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("An error occurred: " + ex.Message);
+        //    }
+        //}
+
+        public static int CountLinesInCsvFile(string filepath)
+        {
+            int count = 0;
 
             try
             {
-                wb = app.Workbooks.Open(path);
-                ws = wb.Worksheets["sheet1"];
-
-                string cellData = " " + ws.Range["A1"].Value;
-                Console.WriteLine(cellData);
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        count++;
+                    }
+                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                Console.WriteLine(ex.Message);
             }
-            
 
-            Console.ReadLine();
+            return count;
         }
-      
+
+
     }
 }
