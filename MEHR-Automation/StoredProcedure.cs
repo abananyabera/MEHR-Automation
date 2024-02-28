d04ef11a7ee095c61888f6f14868d94f0f2270be
Ananya Bera <ananya.bera@corteva.com>
2024-02-28 18:36:44 +0530

    added specialChar stored proc

--- MEHR-Automation/StoredProcedure.cs
+++ MEHR-Automation/StoredProcedure.cs
@@ -27,10 +27,10 @@ namespace MEHR_Automation
             #region MyRegion
             //Console.WriteLine("\n stored procedure procProcessEmployeeUpdates started\n");
             //string procProcessEmployeeUpdates_Query = "exec procProcessEmployeeUpdates";
-            //SqlDataReader datareader9 = executeQueries.ExecuteQuery(procProcessEmployeeUpdates_Query, sqlconnection);
-            //while (datareader9.Read())
+            //SqlDataReader dataReader = executeQueries.ExecuteQuery(procProcessEmployeeUpdates_Query, sqlconnection);
+            //while (dataReader.Read())
             //{
-            //    Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+            //    Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             //}
             //Console.WriteLine("\nstored procedure procProcessEmployeeUpdates is Executed\n");
 
@@ -41,11 +41,11 @@ namespace MEHR_Automation
         {
 
             Console.WriteLine("\n stored procedure procUpdateSolaeJobSubgroup_tempfix started\n");
-            string Query9 = "exec procUpdateSolaeJobSubgroup_tempfix";
-            SqlDataReader datareader9 = executeQueries.ExecuteQuery(Query9, sqlconnection);
-            while (datareader9.Read())
+            string Query = "exec procUpdateSolaeJobSubgroup_tempfix";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
             {
-                Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             }
             Console.WriteLine("\nstored procedure procUpdateSolaeJobSubgroup_tempfix is Executed\n");
         }
@@ -53,11 +53,11 @@ namespace MEHR_Automation
         public void procPostUpdateStoredProcedure(SqlConnection sqlconnection)
         {
             Console.WriteLine("\n stored procedure proc_Post_Update_Processing started\n");
-            string Query9 = "exec proc_Post_Update_Processing";
-            SqlDataReader datareader9 = executeQueries.ExecuteQuery(Query9, sqlconnection);
-            while (datareader9.Read())
+            string Query = "exec proc_Post_Update_Processing";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
             {
-                Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             }
             Console.WriteLine("\nstored procedure proc_Post_Update_Processing is Executed\n");
 
@@ -66,11 +66,11 @@ namespace MEHR_Automation
         public void procRemoveInvalidEmailStoredProcedure(SqlConnection sqlconnection)
         {
             Console.WriteLine("\n stored procedure procRemoveInvalidEmail started\n");
-            string Query9 = "exec dbo.procRemoveInvalidEmail";
-            SqlDataReader datareader9 = executeQueries.ExecuteQuery(Query9, sqlconnection);
-            while (datareader9.Read())
+            string Query = "exec dbo.procRemoveInvalidEmail";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
             {
-                Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             }
             Console.WriteLine("\nstored procedure procRemoveInvalidEmail is Executed\n");
 
@@ -79,11 +79,11 @@ namespace MEHR_Automation
         public void procRefineManagerDataStoredProcedure(SqlConnection sqlconnection)
         {
             Console.WriteLine("\n stored procedure procRefineManagerData started\n");
-            string Query9 = "exec procRefineManagerData";
-            SqlDataReader datareader9 = executeQueries.ExecuteQuery(Query9, sqlconnection);
-            while (datareader9.Read())
+            string Query = "exec procRefineManagerData";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
             {
-                Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             }
             Console.WriteLine("\nstored procedure procRefineManagerData is Executed\n");
 
@@ -92,11 +92,11 @@ namespace MEHR_Automation
         public void procOverrideBadUpdatesStoredProcedure(SqlConnection sqlconnection)
         {
             Console.WriteLine("\n stored procedure procOverrideBadUpdates started\n");
-            string Query9 = "exec dbo.procOverrideBadUpdates";
-            SqlDataReader datareader9 = executeQueries.ExecuteQuery(Query9, sqlconnection);
-            while (datareader9.Read())
+            string Query = "exec dbo.procOverrideBadUpdates";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
             {
-                Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             }
             Console.WriteLine("\nstored procedure procOverrideBadUpdates is Executed\n");
 
@@ -105,11 +105,11 @@ namespace MEHR_Automation
         public void procStage1ReviewCleanupStoredProcedure(SqlConnection sqlconnection)
         {
             Console.WriteLine("\n stored procedure procStage1ReviewCleanup started\n");
-            string Query9 = "exec procStage1ReviewCleanup";
-            SqlDataReader datareader9 = executeQueries.ExecuteQuery(Query9, sqlconnection);
-            while (datareader9.Read())
+            string Query = "exec procStage1ReviewCleanup";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
             {
-                Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             }
             Console.WriteLine("\nstored procedure procStage1ReviewCleanup is Executed\n");
 
@@ -118,13 +118,55 @@ namespace MEHR_Automation
         public void procUpdatePicklistValuesStoredProcedure(SqlConnection sqlconnection)
         {
             Console.WriteLine("\n stored procedure procUpdatePicklistValues started\n");
-            string Query9 = "exec procUpdatePicklistValues";
-            SqlDataReader datareader9 = executeQueries.ExecuteQuery(Query9, sqlconnection);
-            while (datareader9.Read())
+            string Query = "exec procUpdatePicklistValues";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
             {
-                Console.WriteLine(datareader9[0] + "|" + datareader9[1]);
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
             }
             Console.WriteLine("\nstored procedure procUpdatePicklistValues is Executed\n");
+
+        }
+
+        public void replaceSpecialChar(SqlConnection sqlconnection)
+        {
+            Console.WriteLine("\n stored procedure replaceSpecialChar started\n");
+            string Query = "exec Proc_ReplaceSpecialChar";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
+            {
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
+            }
+            Console.WriteLine("\nstored procedure replaceSpecialChar is Executed\n");
+
+        }
+
+        public void findSpecialChar(SqlConnection sqlconnection)
+        {
+            Console.WriteLine("\n stored procedure findSpeciaChar started\n");
+            string Query = "exec find_Specialchar";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            Console.WriteLine("{0,-15} | {1,-15} | {2,-15} | {3,-15}",dataReader.GetName(0), dataReader.GetName(1), dataReader.GetName(2), dataReader.GetName(3));
+            while (dataReader.Read())
+            {
+                Console.WriteLine("{0,-15} | {1,-15} | {2,-15} | {3,-15}", dataReader[0], dataReader[1], dataReader[2], dataReader[3]);
+
+            }
+            Console.WriteLine("\nstored procedure findSpecialChar is Executed\n");
+
+        }
+
+        //Will execute al last after finishing everything
+        public void procUpdateMasterEmployeeTable(SqlConnection sqlconnection)
+        {
+            Console.WriteLine("\n stored procedure procUpdateMasterEmployeeTable started\n");
+            string Query = "exec procUpdateMasterEmployeeTable";
+            SqlDataReader dataReader = executeQueries.ExecuteQuery(Query, sqlconnection);
+            while (dataReader.Read())
+            {
+                Console.WriteLine(dataReader[0] + "|" + dataReader[1]);
+            }
+            Console.WriteLine("\nstored procedure procUpdateMasterEmployeeTable is Executed\n");
 
         }
 
